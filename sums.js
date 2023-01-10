4e58c2784b5a6c3d7a74335be36257269233c259
October45 <vikto45.t@gmail.com>
2023-01-10 14:05:38 +0200

    quest 2

---
+++ sums.js
@@ -0,0 +1,5 @@
+// Create a function named sums that accepts a number and returns its partitions.
+
+// A partition is a group of numbers, where the sum of the partition is equal to the number argument.
+// Duplicate partitions are not allowed. [1, 2] and [2, 1] are considered duplicates. The array of partitions must be sorted.
+function sums(num) {}
