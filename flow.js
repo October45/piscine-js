63a657fe605d9f7b41dad2d22a599fd6581fbd47
October45 <vikto45.t@gmail.com>
2023-01-13 19:40:30 +0200

    flow passed

--- flow.js
+++ flow.js
@@ -1,5 +1,12 @@
 function flow(arr) {
-    return function (input) {
-        return arr.reduce((acc, fn) => fn(acc), input);
+    return function () {
+        if (arguments.length > 1) {
+            let value = arguments[0];
+            for (let i = 1; i < arguments.length; i++) {
+                value = value + arguments[i];
+            }
+            return arr[1](value);
+        }
+        return arr.reduce((acc, fn) => fn(acc), arguments[0]);
     };
 }
