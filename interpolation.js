148b744e04d8ef66eb91b5e8d57ac7440ffb59a0
October45 <vikto45.t@gmail.com>
2023-01-18 10:59:22 +0200

    tihi goran done now :D

--- interpolation.js
+++ interpolation.js
@@ -1,13 +1,3 @@
-// Create a function named interpolation that takes an object with 5 properties: step, start, end, callback and duration.
-
-// This function must interpolate points from the start position to the end position (not including the end position). The number of points depends on the number of steps.
-
-// For each interpolation point, you must call the callback function with an array of the two points [x, y]:
-
-// x: distance
-// y: point
-// There should be a delay between each callback invocation of duration / step, so that the final call happens after duration.
-
 function interpolation({
     step = 0,
     start = 0,
