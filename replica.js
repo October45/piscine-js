068debeeaea62069ba985078fe4ab4907021be0f
October45 <vikto45.t@gmail.com>
2023-01-23 10:49:01 +0200

    goran code sux

--- replica.js
+++ replica.js
@@ -1,3 +1,13 @@
+var is = {};
+is.arr = (n) => Array.isArray(n);
+is.obj = (n) =>
+    typeof n === 'object' &&
+    !is.fun(n) &&
+    !is.arr(n) &&
+    n !== null &&
+    !(n instanceof RegExp);
+is.fun = (n) => typeof n === 'function';
+
 function replica(target, ...sources) {
     sources.forEach((source) => {
         Object.keys(source).forEach((key) => {
